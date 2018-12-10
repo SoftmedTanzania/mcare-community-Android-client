65cc675baef51b0b070125bdf9a589bc45a5106c
silasonyango93 <silas.onyango93@gmail.com>
2018-12-10 16:31:33 +0300

    Chw login

---
+++ app/src/main/java/com/softmed/tanzania/referral/Config.java
@@ -0,0 +1,6 @@
+package com.softmed.tanzania.referral;
+
+public class Config {
+    public static final String ip = "http://192.168.43.58:5000/";
+    public static final String login_url =ip + "user_login";
+}
