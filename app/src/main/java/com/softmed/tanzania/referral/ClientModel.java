97ddc4f4f0fe78a06f258a381b6c3d3d304e4a71
silasonyango93 <silas.onyango93@gmail.com>
2018-12-15 12:41:52 +0300

    Update

---
+++ app/src/main/java/com/softmed/tanzania/referral/ClientModel.java
@@ -0,0 +1,30 @@
+package com.softmed.tanzania.referral;
+
+public class ClientModel {
+
+    String ClientId,UserId,FirstName,MiddleName,SurName,PhoneNumber,Email,PhysicalAddress,DOB,Gender,VillageId,VillageName,WardId,WardName,VillageRefNo,WardRefNo,IsAChildOf;
+
+    public ClientModel(String ClientId,String UserId,String FirstName,String MiddleName,String SurName,String PhoneNumber,String Email,String PhysicalAddress,String DOB,String Gender,String VillageId,String VillageName,String WardId,String WardName,String VillageRefNo,String WardRefNo,String IsAChildOf){
+        this.ClientId=ClientId;
+        this.UserId=UserId;
+        this.FirstName=FirstName;
+        this.MiddleName=MiddleName;
+        this.SurName=SurName;
+        this.PhoneNumber=PhoneNumber;
+        this.Email=Email;
+        this.PhysicalAddress=PhysicalAddress;
+        this.DOB=DOB;
+        this.Gender=Gender;
+        this.VillageId=VillageId;
+        this.VillageName=VillageName;
+        this.VillageName=VillageName;
+        this.WardId=WardId;
+        this.WardName=WardName;
+        this.VillageRefNo=VillageRefNo;
+        this.WardRefNo=WardRefNo;
+        this.IsAChildOf=IsAChildOf;
+
+
+
+    }
+}
